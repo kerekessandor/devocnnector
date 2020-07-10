ef416ace6efdbf2c7c822de0d4307b5abbe62d3c
Kerekes Sandor <kerekessandor@yahoo.com>
2020-07-10 15:30:05 +0300

    Delete Profile and User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,4 +137,23 @@ router.get("/user/:user_id", checkObjectId('user_id'), async (req, res) => {
     }
 });
 
+// @route   Delete api/profile/
+// @desc    Delete a profile, user & posts
+// @access  Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        //delete profile
+        await Profile.findOneAndRemove({user: req.user.id});
+
+        //delete user
+        await User.findOneAndRemove({_id: req.user.id});
+
+        res.json({msg: 'User removed'});
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
